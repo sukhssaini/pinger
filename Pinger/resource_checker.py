55bd2eb83adb669a0b1121ba015dacaa99d7a741
sukhssaini <sainisukha88@gmail.com>
2018-02-10 16:17:45 +0530

    updated resource_checker
    - removed resource_checker
    - renamed resource_checker1 to resource_checker
    - new resource checker is implemented as class

--- Pinger/resource_checker.py
+++ Pinger/resource_checker.py
@@ -1,15 +1,28 @@
 import requests
+from logger import logger
 
 
-def ping_link(link):
-    request = requests.get(link)
-    if request.status_code == 200:
-        return True
-    else:
-        return False
+# pending implementation of hitting webhook on error
+class ResourceChecker:
+    def __init__(self):
+        self.links = None
+        self.webhook_url = None
 
+    def check_resource(self, link):
+        logger.info('checking link status {0}'.format(link))
+        request = requests.get(link)
+        if request.status_code == 200:
+            return True
+        else:
+            return False
 
-def check_health(links):
-    for link in links:
-        is_live = ping_link(link)
-        print("{0} link status isActive {1}".format(link, is_live))
+    def check_resources(self):
+        if (self.links is not None) and (len(self.links) > 0):
+            for link in self.links:
+                status_ok = self.check_resource(link)
+                if status_ok:
+                    logger.info("'{0}' link is healthy.".format(link))
+                else:
+                    logger.error("'{0}' link is not healthy.".format(link))
+        else:
+            logger.info("No health check links found.")
